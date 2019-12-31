5da70ddbd57c86e516a46c5f823c6cd18aaa006a
wanhar <wanhar@sehatq.com>
2019-12-31 15:12:03 +0700

    Initial Commit

---
+++ Find My Age/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find My Age
+//
+//  Created by Wanhar on 31/12/19.
+//  Copyright © 2019 Karya anak Takalar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
